ae3b98719dcee9c49855e06033f1e99717c51f67
chry902 <tchry902@gmail.com>
2021-10-25 17:18:37 +0200

    creazione ciclo

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,21 @@
-const card = [
+const userList = [
 
     {
-        user: `Mario Rossi`,
+        userName: `Mario Rossi`,
+        paragraph: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo a obcaecati modi fuga, animi non excepturi, nemo nobis facilis doloremque rem suscipit qui corporis ipsam beatae! Quis provident illo atque.`,
+        image: `https://i.picsum.photos/id/215/300/300.jpg?hmac=fbcOUjHvJU-TDq0Lsbc_lPoR8aKijF4nXbuUlKTDvKw`,
+        numeroLikes: `0`,
+        dataCreazione: `0`,
+    },
+    {
+        userName: `Francesco Grigio`,
+        paragraph: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo a obcaecati modi fuga, animi non excepturi, nemo nobis facilis doloremque rem suscipit qui corporis ipsam beatae! Quis provident illo atque.`,
+        image: `https://i.picsum.photos/id/215/300/300.jpg?hmac=fbcOUjHvJU-TDq0Lsbc_lPoR8aKijF4nXbuUlKTDvKw`,
+        numeroLikes: `0`,
+        dataCreazione: `0`,
+    },
+    {
+        userName: `Mario Draghi`,
         paragraph: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo a obcaecati modi fuga, animi non excepturi, nemo nobis facilis doloremque rem suscipit qui corporis ipsam beatae! Quis provident illo atque.`,
         image: `https://i.picsum.photos/id/215/300/300.jpg?hmac=fbcOUjHvJU-TDq0Lsbc_lPoR8aKijF4nXbuUlKTDvKw`,
         numeroLikes: `0`,
@@ -9,6 +23,32 @@ const card = [
     }
 
 
+
+
 ]
 
-for
+for (let index = 0; index < userList.length; index++) {
+    const member = userList[index];
+    const element = `
+    
+    <div class="card ">
+            <h3>${userName}</h3>
+
+            <p>${paragraph}</p>
+
+            <div>${dataCreazione}</div>
+
+            <div>
+                <img src="${member.image}" alt="">
+            </div>
+
+            <div>${member.numeroLikes}</div>
+
+            
+
+        </div>
+    `
+
+    document.querySelector(`.row`).insertAdjacentHTML(`beforeend`, element)
+    console.log(document.querySelector(`.row`));
+}
