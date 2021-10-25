e19e7a3425530863e48dc6c163a34a9f29ecbcc8
chry902 <tchry902@gmail.com>
2021-10-25 16:47:38 +0200

    creazione card

---
+++ assets/main.js
@@ -0,0 +1,14 @@
+const card = [
+
+    {
+        user: `Mario Rossi`,
+        paragraph: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo a obcaecati modi fuga, animi non excepturi, nemo nobis facilis doloremque rem suscipit qui corporis ipsam beatae! Quis provident illo atque.`,
+        image: `https://i.picsum.photos/id/215/300/300.jpg?hmac=fbcOUjHvJU-TDq0Lsbc_lPoR8aKijF4nXbuUlKTDvKw`,
+        numeroLikes: `0`,
+        dataCreazione: `0`,
+    }
+
+
+]
+
+for
